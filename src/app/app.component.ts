972aabea81c15d0fd5068238830e6552ba74f4e6
Sraavan Chevireddy <schevireddy@saventech.com>
2021-01-06 17:13:04 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { ToDo } from "./todo";
+import { VirtualTimeScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-tutorial';
+  title = "Hello World Tutorial";
+
+      list: ToDo[] = [];
+      todoValue = "";
+
+  // Add Items to list..
+    addItem(){
+
+        if(this.todoValue !== ""){
+          const newItem: ToDo = {
+            id : Date.now(),
+            value : this.todoValue,
+            isDone : false
+          };
+          this.list.push(newItem);
+        }
+        this.todoValue = "";
+    }
+  
+  deleteItem(fromId:number){
+    this.list = this.list.filter(item => item.id !== fromId);
+  }
+
+
+
 }
